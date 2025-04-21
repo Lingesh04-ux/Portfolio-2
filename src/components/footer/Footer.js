990494bde0f882223abc73a8b084553f2c97d4a4
Lingesh <111057785+Lingesh04-ux@users.noreply.github.com>
2025-04-21 13:31:49 +0530

    Update Footer.js
    update the rights reseved "Cooperation" into "Corporation"

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("© 2025 all rights reserved to Wind Sea Cooperation.")}
+          {emoji("© 2025 all rights reserved to Wind Sea Corporation.")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
